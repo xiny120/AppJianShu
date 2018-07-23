d14cd2a9120aa01297ad0f5f75dbe551a90dccc6
毛毛 <20774746@qq.com>
2018-07-23 15:31:00 +0800

    初始化meshow
    新建meshow工程

---
+++ MeShow/MeShow/MeShow/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using MeShow.Models;
+
+namespace MeShow.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
