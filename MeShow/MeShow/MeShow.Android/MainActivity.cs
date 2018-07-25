3b98ec4917118239fd7795620bf8e5f741d5473d
毛毛 <20774746@qq.com>
2018-07-25 16:59:56 +0800

    views
    <contengpage>
    <local:>

--- MeShow/MeShow/MeShow.Android/MainActivity.cs
+++ MeShow/MeShow/MeShow.Android/MainActivity.cs
@@ -27,6 +27,7 @@ namespace MeShow.Droid
 
             global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
             LoadApplication(new App());
+            
         }
 
         public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
