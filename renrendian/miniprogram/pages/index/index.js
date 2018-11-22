7a0963c32e12992289aee7a632b823595c799e51
毛毛 <20774746@qq.com>
2018-11-22 17:15:11 +0800

    d'f'g'd'f
    豆腐干地方豆腐干豆腐干地方

--- renrendian/miniprogram/pages/index/index.js
+++ renrendian/miniprogram/pages/index/index.js
@@ -7,7 +7,21 @@ Page({
     userInfo: {},
     logged: false,
     takeSession: false,
-    requestResult: ''
+    requestResult: '',
+    imgUrls: [
+      ['https://developers.weixin.qq.com/miniprogram/dev/image/cat/0.jpg','goodsid0'],//图片src
+      ['../../images/ad01.png', 'goodsid1'],
+    ],
+    autoplay: true,//是否自动播放
+    autoplaytxt: "停止自动播放",
+    indicatorDots: true,//指示点
+    // indicator-color："white",//指示点颜色 暂未启动
+    // indicator-active-color："red",//当前选中的指示点颜色暂未启动
+    indicatorDotstxt: "隐藏指示灯",
+    interval: 5000,//图片切换间隔时间
+    duration: 500,//每个图片滑动速度,
+    circular: true,//是否采用衔接滑动
+    current: 0,//初始化时第一个显示的图片 下标值（从0）index
   },
 
   onLoad: function() {
@@ -116,5 +130,32 @@ Page({
       }
     })
   },
+
+  swiperitemtap:function(event){
+    const detailuri = '../goods/detail?id=' + event.target.dataset.aduri;
+    console.log(detailuri)
+    wx.navigateTo({
+      url: detailuri,
+    })
+  },
+
+  autoplaychange: function (event) {//停止、播放按钮
+
+    if (this.data.autoplaytxt == "停止自动播放") {
+      this.setData({
+        autoplaytxt: "开始自动播放",
+        autoplay: !this.data.autoplay
+      })
+    } else {
+      this.setData({
+        autoplaytxt: "停止自动播放",
+        autoplay: !this.data.autoplay
+      })
+    };
+
+  },
+  imgchange: function (e) {//监听图片改变函数
+    console.log(e.detail.current)//获取当前显示图片的下标值
+  }  
 
 })
