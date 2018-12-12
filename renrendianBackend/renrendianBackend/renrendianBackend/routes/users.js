acd3743b4afc3b3caec883e17e15221d55d852d6
毛毛 <20774746@qq.com>
2018-12-12 16:43:28 +0800

    士大夫撒
    首发式地方

--- renrendianBackend/renrendianBackend/renrendianBackend/routes/users.js
+++ renrendianBackend/renrendianBackend/renrendianBackend/routes/users.js
@@ -4,10 +4,15 @@ var fs = require('fs');
 var router = express.Router();
 var app = express();
 
+// 初始化示例
+const tcbapp = require('tcb-admin-node');
+
+
+
 /* GET users listing. */
 router.get('/', function (req, res) {
 
-    readJson();
+    readJson(res);
 
     res.send('respond with a resource');
 });
@@ -15,7 +20,7 @@ router.get('/', function (req, res) {
 module.exports = router;
 
 
-function readJson() {
+function readJson(res) {
     //现将json文件读出来
     fs.readFile('./setup/qcloud.json', 'utf-8', function (err, data) {
         if (err) {
@@ -25,5 +30,28 @@ function readJson() {
         person = JSON.parse(person);//将字符串转换为json对象
         app.set("SecretId", person.SecretId);
         app.set("SecretKey", person.SecretKey);
+
+        // 初始化资源
+        // 云函数下不需要secretId和secretKey。
+        // env如果不指定将使用默认环境
+        tcbapp.init({ secretId: person.SecretId, secretKey: person.SecretKey, env: 'subrac-89aa62' });
+        tcbapp.config.timeout = 1000 * 5 * 60;
+        const db = tcbapp.database();
+        //res.send("open database! ");
+
+        db.collection('store_bannermenu').where({
+            storeuuid: 'W_UO50XacNtiP6m5'
+        })
+            .get({
+                success: function (res0) {
+                    //console.log(res.data);
+                    res.send(" store_bannermenu ok! ");
+                },
+                fail: function (e) {
+                    //console.log(e);
+                    res.send(e);
+                }
+            });
+
     });
 }
